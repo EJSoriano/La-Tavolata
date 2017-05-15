caeb5dc2fac2d9392678e1f9dc7e85b088de5566
LanceMcVicar <lancemcvicar@gmail.com>
2017-05-15 14:44:06 -0700

    Added a link back to Admin.php.

---
+++ Lance's Stuff/modify.php
@@ -0,0 +1,116 @@
+#!/usr/local/php5/bin/php-cgi
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>La Tavolita</title>
+		<meta charset="utf-8">
+		<link rel = "stylesheet" href="reset.css">
+		<link rel = "stylesheet" href="layout.css">
+	</head>
+	<?php require 'header.php'; 
+	//PHP Session validation here
+	
+	$regprice = "/^\d+(?:\.\d{2})?$/";
+	$regname = "/.*/";
+	$regdesc = "/.*/";
+	$regpath = "/.*/";
+	$regcat = "^\d+$";
+
+
+
+	  if(isset($_GET[errname])) {
+		  $nameErr = "* Must have valid characters in Name. No Special Characters Allowed eg '.' ',' '^' etc"; 
+	  } 
+	  if (isset($_GET[errprice])) {
+		  $priceErr = "* Price must be in valid format. eg '12' or '12.40'"; 
+	  }
+	  if (isset($_GET[errdescription])) {
+		  $descErr = "* Description is required";
+	  }
+	  if (isset($_GET[errpath])) {
+		  $pathErr = "* Path needs to be valid"; 
+	  }
+	  if (isset($_GET[errcat])) {
+		  $catErr = "* You Need to set a Category"; 
+	  }
+	  if (isset($_GET[sqlfail])){
+		  $sqlErr = "SQL Error: " . mysqli_error($connection);
+	  }
+	  if (isset($_GET[success])){
+		  $success = "Your Item was Successfully added";
+	  }
+	  $query = "SELECT name, price, category_id, path_to_image,description from menu_item WHERE name = ?";
+	  if(!$stmt=mysqli_prepare($connection, $query)){
+		  echo "PROBLEMS";
+	  }
+	  if(!mysqli_stmt_bind_param($stmt, 's',$oname)){
+		  echo "Dafuq!";
+	  };
+	  $oname= $_GET[oname];
+	  echo $oname;
+
+	  if(!mysqli_stmt_execute($stmt)){
+		  echo "DAMMIT";
+	  };
+	  if(!mysqli_stmt_bind_result($stmt, $name, $price, $category, $path, $description)){
+		  echo "WHAT THE FUQ";
+	  };
+	  if(!mysqli_stmt_fetch($stmt)){
+		  echo "Illegal Link, Click <a href='admin.php'> Here </a> to go back";
+		  die();
+	  }
+	  mysqli_stmt_close($stmt);
+	?>
+
+	<body>
+		<div class="back"> Click <a href="admin.php"> Here </a> </div>
+		<span class='errorblock'> <?php echo $sqlErr; ?> </span>
+		<span class='successblock'> <?php echo $success; ?> </span>
+		<form method="POST" action='process.php' >
+			<fieldset>
+				<legend> New Menu Item Form </legend>
+					<input type='hidden' name='type' value='modify'>
+					<input type='hidden' name='oname' value="<?php echo $_GET[oname]; ?>"
+					<div class= 'form_name'>	
+						<label for= 'name'>Item Name </label>
+							<input type='text' name='name' value="<?php echo $name ?>">
+							<span class='error'> <?php echo $nameErr; ?> </span>
+					</div>
+					<div class= 'form_price'>
+						<label for = 'price'> Price </label>
+							<input type='text' name='price' value='<?php echo $price ?>'>
+							<span class='error'> <?php echo $priceErr; ?> </span>
+					</div>
+					<div class= 'form_category'>
+						<select name = 'category' id= 'category' >
+						<?php 			
+							$query = "SELECT name, id FROM menu_categories ORDER BY name ASC";
+							$result = mysqli_query($connection, $query);
+							if($result){
+								while($row=mysqli_fetch_assoc($result)){
+									echo "<option value='" . $row[id] ."'"; 
+									if($row[id]==$category){echo " selected='selected'" ;}  
+									echo ">" . $row[name]. "</option>\r\n"; 
+								}
+							}else{echo "Database Query Error Detect, no results returned <br/>";}
+						?>
+						</select>
+						<span class='error'> <?php echo $catErr ?> </span>
+					</div>
+					<div class= 'form_path'>
+						<label for = 'path'>File Path </label>
+							<input type='text' name = 'path'  value='<?php echo $path ?>'>
+							<span class='error'><?php echo $pathErr; ?></span>
+					</div>
+					<div class= 'form_description'>
+						<label for= 'description'>Description </label>
+							<input type='text' name = 'description'  value='<?php echo $description ?>'>
+							<span class='error'><?php echo $descErr; ?></span>			
+					</div>
+						<input type='submit' value='submit'>		
+			</fieldset>
+		</form>
+	</body>
+</html>
+
+<?php mysqli_close();?>
---
+++ Lance's Stuff/process.php
@@ -0,0 +1,166 @@
+#!/usr/local/php5/bin/php-cgi
+<?php
+	//REGEX for Price, Name, Description, Path
+	$regprice = "/^\d+(?:\.\d{2})?$/";
+	$regname = "/^[\w\s\' ]+$/";
+	$regdesc = "/.+/";
+	$regpath = "/.+/";
+	$regcat = "/^\d+$/";
+	$getarray = array();
+	$error=false; 
+	//Connection Info
+	require 'header.php';
+	$type=$_POST[type];
+	
+	
+	function redirectTo($page){
+		$url = $page;
+		global $getarray;
+		if(strpos($page, '?')===false){
+			$url.='?';
+		}
+		foreach($getarray as $value){
+			$url .= '&'.$value . '=true';
+		}
+		$host  = $_SERVER['HTTP_HOST'];
+		$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+		$redirect = "Location: http://".$host.$uri.'/'.$url;
+		header("Location: http://".$host.$uri.'/'.$url);
+
+		die();
+	}
+	function sanitize($data)
+	{
+		$data = trim($data);
+		$data= stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data; 
+	}
+	
+	switch($type){
+		case "add":
+			if(!preg_match($regname, $_POST[name])){
+				$getarray[]= "errname";
+				$error=true;
+			}
+			if(!preg_match($regprice, $_POST[price])){
+				$getarray[]= 'errprice';
+				$error=true;
+			}
+			if(!preg_match($regdesc, $_POST[description])){
+				$getarray[]= 'errdescription';
+				$error=true;
+			}
+			if(!preg_match($regpath, $_POST[path])){
+				$getarray[]= 'errpath';
+				$error=true;
+			}
+			if(!preg_match($regcat, $_POST[category])){
+				$getarray[]= 'errcat';
+				$error=true;
+			}
+			if($error!=true){
+				//Attempt Query
+				echo "Got here";
+				$query = "INSERT INTO menu_item (name,price,description,category_id,path_to_image)VALUES(?,?,?,?,?)";
+				$stmt=mysqli_prepare($connection, $query);
+				mysqli_stmt_bind_param($stmt, 'sssss',$name,$price,$description,$category,$path); 
+				$name=$_POST[name];
+				$price=$_POST[price];
+				$description=$_POST[description];
+				$category=$_POST[category];
+				$path=$_POST[path];
+				
+				if(mysqli_stmt_execute($stmt)==true){
+					//Query Successful
+					$getarray[] = 'success';
+					redirectTo("add.php");
+				}else{
+					//Query Unsuccessful					
+					$getarray[] = 'sqlfail';
+					redirectTo("add.php");
+				}
+				mysqli_stmt_close($stmt);			
+			}else{
+				//Put all error's in the URL
+				redirectTo("add.php");
+			}
+			break;
+		case "modify":
+			echo "Modify";
+			if(!preg_match($regname, $_POST[name])){
+				$getarray[]= "errname";
+				$error=true;
+			}
+			if(!preg_match($regprice, $_POST[price])){
+				$getarray[]= 'errprice';
+				$error=true;
+			}
+			if(!preg_match($regdesc, $_POST[description])){
+				$getarray[]= 'errdescription';
+				$error=true;
+			}
+			if(!preg_match($regpath, $_POST[path])){
+				$getarray[]= 'errpath';
+				$error=true;
+			}
+			if(!preg_match($regcat, $_POST[category])){
+				$getarray[]= 'errcat';
+				$error=true;
+			}
+			if(!preg_match($regname, $_POST[oname])){
+				$getarray[]= "erroname";
+				$error=true;
+			}
+			if($error!=true){
+				//Attempt Query
+				echo "Got here";
+				$query = "UPDATE menu_item SET name=?, price=?,description=?, category_id=?, path_to_image=? WHERE name=?";
+				$stmt=mysqli_prepare($connection, $query);
+				mysqli_stmt_bind_param($stmt, 'ssssss',$name,$price,$description,$category,$path,$oname); 
+				$name=$_POST[name];
+				$price=$_POST[price];
+				$description=$_POST[description];
+				$category=$_POST[category];
+				$path=$_POST[path];
+				$oname=$_POST[oname];
+				if(mysqli_stmt_execute($stmt)==true){
+					//Query Successful
+					$getarray[] = 'success';
+					redirectTo("admin.php");
+				}else{
+					//Query Unsuccessful					
+					$getarray[] = 'sqlfail';
+					redirectTo("modify.php?oname=".trim($oname));
+				}
+				mysqli_stmt_close($stmt);			
+			}else{
+				//Put all error's in the URL
+				redirectTo("modify.php?oname=".trim($oname));
+			}
+			break;
+				
+			break;
+		case "delete":
+			$query = "DELETE FROM menu_item WHERE name= ?";
+			$stmt=mysqli_prepare($connection, $query);
+			mysqli_stmt_bind_param($stmt, 's',$name); 
+			$name= $_POST[name];
+			
+			if(mysqli_stmt_execute($stmt)==true){
+				//Query Successful
+				$getarray[] = 'success';
+				redirectTo("admin.php");
+			}
+			else{
+				//Query Unsuccessful
+				$getarray[] = 'sqlfail';
+				redirectTo("admin.php");
+			}
+			mysqli_stmt_close($stmt);			
+			break;
+		default: 
+			echo "How did you get here? Go Away";
+			break;
+	}
+?>
